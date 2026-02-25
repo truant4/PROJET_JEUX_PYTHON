978d82f6bb87b37fe3000548a60d140fbb05ff6d
Yann <yann.popov@hotmail.fr>
2026-02-25 20:05:49 +0100

    création première partie de la forêt plus début du village

---
+++ assets/tiles/Roofs_house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Roofs_house" tilewidth="16" tileheight="16" tilecount="625" columns="25">
+ <image source="../Pixel Crawler - Free Pack 2.0.4/Environment/Structures/Buildings/Roofs.png" width="400" height="400"/>
+</tileset>
---
+++ assets/tiles/house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="house" tilewidth="16" tileheight="16" tilecount="625" columns="25">
+ <image source="../Pixel Crawler - Free Pack 2.0.4/Environment/Structures/Buildings/Props.png" width="400" height="400"/>
+</tileset>
