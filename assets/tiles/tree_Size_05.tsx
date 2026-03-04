147bc60770f732586b0757d9248214b5bc763a1e
Yann <yann.popov@hotmail.fr>
2026-03-04 12:18:16 +0100

    modif de la map

---
+++ assets/tiles/tree_Size_05.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tree_Size_05" tilewidth="16" tileheight="16" tilecount="644" columns="28">
+ <image source="../Pixel Crawler - Free Pack 2.0.4/Environment/Props/Static/Trees/Model_01/Size_05.png" width="448" height="368"/>
+</tileset>
