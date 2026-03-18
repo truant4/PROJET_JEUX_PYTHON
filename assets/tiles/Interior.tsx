334bfde73403627458767a876071ae0629feadfa
Julien_vlm <julienvolmerange91@gmail.com>
2026-03-18 19:15:44 +0100

    Ajout dongeon et Maisons

---
+++ assets/tiles/Interior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Interior" tilewidth="16" tileheight="16" tilecount="378" columns="14">
+ <image source="../main-characters-home-free-top-down-pixel-art-asset/Tiled_files/Interior.png" width="224" height="432"/>
+</tileset>
